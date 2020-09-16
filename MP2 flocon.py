14fd2cb71d180d99e277c6ff4cd2b771e18ead1b
LoannNicotte <65536418+LoannNicotte@users.noreply.github.com>
2020-09-16 15:30:45 +0200

    Update MP2 flocon.py

--- MP2 flocon.py
+++ MP2 flocon.py
@@ -1,42 +1,82 @@
 import turtle 
-d = 7
 
-turtle.penup()
-turtle.goto(-270,0)
-turtle.speed(0)
-turtle.pendown()
-for i in range(2):
+def flocon(d=7):
+    turtle.penup()
+    turtle.goto(-270,0)
+    turtle.speed(0)
+    turtle.pendown()
     for i in range(2):
-        for i in range (2):
-            for i in range(2):
+        for i in range(2):
+            for i in range (2):
                 for i in range(2):
-                    turtle.forward(d)
-                    turtle.lt(60)
-                    turtle.forward(d)
-                    turtle.rt(120)
-                    turtle.forward(d)
-                    turtle.lt(60)
-                    turtle.forward(d)
-                    turtle.lt(60)
+                    for i in range(2):
+                        turtle.forward(d)
+                        turtle.lt(60)
+                        turtle.forward(d)
+                        turtle.rt(120)
+                        turtle.forward(d)
+                        turtle.lt(60)
+                        turtle.forward(d)
+                        turtle.lt(60)
+                    turtle.rt(180)
                 turtle.rt(180)
             turtle.rt(180)
         turtle.rt(180)
-    turtle.rt(180)
-    for i in range(2):
-        for i in range (2):
-            for i in range(2):
+        for i in range(2):
+            for i in range (2):
                 for i in range(2):
-                    turtle.forward(d)
-                    turtle.lt(60)
-                    turtle.forward(d)
-                    turtle.rt(120)
-                    turtle.forward(d)
-                    turtle.lt(60)
-                    turtle.forward(d)
-                    turtle.lt(60)
+                    for i in range(2):
+                        turtle.forward(d)
+                        turtle.lt(60)
+                        turtle.forward(d)
+                        turtle.rt(120)
+                        turtle.forward(d)
+                        turtle.lt(60)
+                        turtle.forward(d)
+                        turtle.lt(60)
+                    turtle.rt(180)
                 turtle.rt(180)
             turtle.rt(180)
-        turtle.rt(180)
-            
-  
-turtle.exitonclick()
+    turtle.exitonclick()
+    
+def flocon_full(d=6):
+    turtle.penup()
+    turtle.goto(-250,142)
+    turtle.speed(0)
+    turtle.pendown()
+    for i in range(3):
+        for i in range(2):
+            for i in range(2):
+                for i in range (2):
+                    for i in range(2):
+                        for i in range(2):
+                            turtle.forward(d)
+                            turtle.lt(60)
+                            turtle.forward(d)
+                            turtle.rt(120)
+                            turtle.forward(d)
+                            turtle.lt(60)
+                            turtle.forward(d)
+                            turtle.lt(60)
+                        turtle.rt(180)
+                    turtle.rt(180)
+                turtle.rt(180)
+            turtle.rt(180)
+            for i in range(2):
+                for i in range (2):
+                    for i in range(2):
+                        for i in range(2):
+                            turtle.forward(d)
+                            turtle.lt(60)
+                            turtle.forward(d)
+                            turtle.rt(120)
+                            turtle.forward(d)
+                            turtle.lt(60)
+                            turtle.forward(d)
+                            turtle.lt(60)
+                        turtle.rt(180)
+                    turtle.rt(180)
+                turtle.rt(180)
+    turtle.exitonclick()
+        
+flocon_full()
